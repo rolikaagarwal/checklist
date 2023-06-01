04f50dd9dd3be2bb2d04c0f9453204ab7533f324
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-06-02 04:21:21 +0530

    delete todo

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -11,32 +11,41 @@ function AddItems() {
       setTodo("");
     }
   }
+  const deleteTodo = (text) =>{
+    const newTodo = todos.filter((todo)=> {
+        return todo!==text;
+    });
+    setTodos(newTodo);
+
+  };
   return (
-    <div className="enter-item">
-      <input
-        type="text"
-        placeholder="add item"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-      />
-      <input type="button" value="add" onClick={addTodo} />
+    <div>
+      <div className="enter-item">
+        <input
+          type="text"
+          placeholder="add item"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <input type="button" value="add" onClick={addTodo} />
+      </div>
 
-      <div>
-      {todos.length > 0 ? (
-        <ul className="todo-list">
-          {todos.map((todo, index) => (
-            <div className="todo">
-              <li key={index}> {todo} </li>
+      <div className="display-list">
+        {todos.length > 0 ? (
+          <ul className="todo-list">
+            {todos.map((todo, index) => (
+              <div className="todo">
+                <li key={index}> {todo} </li>
 
-              <button className="delete-button">Delete</button>
-            </div>
-          ))}
-        </ul>
-      ) : (
-        <div className="empty">
-          <p>No task found</p>
-        </div>
-      )}
+                <button className="delete-button" onClick={()=> deleteTodo(todo)}>Delete</button>
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <div className="empty">
+            <p>No task found</p>
+          </div>
+        )}
       </div>
     </div>
   );
